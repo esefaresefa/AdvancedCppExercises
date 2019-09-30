5a31e727e8dc6206e1928c3b9d8d3c1802b0cc69
mattia <biggeb4@gmail.com>
2019-09-30 16:19:25 +0200

    added cout in tests

--- AdvancedC++-Exercise2/main.cpp
+++ AdvancedC++-Exercise2/main.cpp
@@ -5,7 +5,9 @@
 using namespace slist;
 int main()
 {
+	std::cout << std::endl << "default constructor ...";
 	SList<int> list;
+	std::cout << std::endl << "push back ...";
 	list.push_back(111);
 	list.push_back(222);
 	list.push_back(333);
@@ -17,9 +19,12 @@ int main()
 	assert(*biterator++ == 333);
 	std::cout << "OK!" << std::endl;
 
+
+	std::cout << std::endl << "fill constructor default ...";
 	SList<int>list2(12);
 	assert(list2.size()==12);
 
+	std::cout << std::endl << "fill constructor with val ...";
 	SList<int>list3(12,5);
 	assert(list3.size() == 12);
 	for (SList<int>::iterator i = list3.begin(); i != list3.end(); ++i)
@@ -27,6 +32,8 @@ int main()
 		assert(*i == 5);
 	}
 
+
+	std::cout << std::endl << "iterator constructor ...";
 	SList<int>list4(++list3.begin(), list3.end());
 	assert(list4.size() == 11);
 	for (SList<int>::iterator i = list4.begin(); i != list4.end(); ++i)
@@ -35,6 +42,7 @@ int main()
 	}
 
 
+	std::cout << std::endl << "copy constructor ...";
 	SList<int>list5(list4);
 	assert(list5.size() == 11);
 	for (SList<int>::iterator i = list5.begin(); i != list5.end(); ++i)
@@ -42,6 +50,7 @@ int main()
 		assert(*i == 5);
 	}
 
+	std::cout << std::endl << "move constructor ...";
 	SList<int>list6(std::move(list4));
 	assert(list6.size() == 11);
 	for (SList<int>::iterator i = list6.begin(); i != list6.end(); ++i)
@@ -50,6 +59,7 @@ int main()
 	}
 	assert(list4.size() == 0);
 
+	std::cout << std::endl << "inizializer list constructor ...";
 	SList<int>list7{0,1,2,3,4};
 	assert(list7.size() == 5);
 	int val = 0;
@@ -58,12 +68,16 @@ int main()
 		assert(*i == val);
 	}
 
+	std::cout << std::endl << "destructor ...";
 	list7.~SList<int>();
+	std::cout << std::endl << "size ...";
 	assert(list7.size() == 0);
+	std::cout << std::endl << "empty on empty ...";
 	assert(list7.empty());
 
 
 
+	std::cout << std::endl << "operator= ...";
 	SList<int>list8=list6;
 	assert(list8.size() == 11);
 	for (SList<int>::iterator i = list8.begin(); i != list8.end(); ++i)
@@ -71,6 +85,7 @@ int main()
 		assert(*i == 5);
 	}
 
+	std::cout << std::endl << "operator= move ...";
 	SList<int>list9=std::move(list6);
 	assert(list9.size() == 11);
 	for (SList<int>::iterator i = list9.begin(); i != list9.end(); ++i)
@@ -80,6 +95,7 @@ int main()
 	assert(list6.size() == 0);
 
 
+	std::cout << std::endl << "operator= inizializer list ...";
 	SList<int>list10={ 0,1,2,3,4 };
 	assert(list10.size() == 5);
 	int val1 = 0;
@@ -88,8 +104,10 @@ int main()
 		assert(*i == val1);
 	}
 
+	std::cout << std::endl << "not empty ...";
 	assert(!list10.empty());
 
+	std::cout << std::endl << "push front ...";
 	list10.push_front (-1);
 	assert(list10.size() == 6);
 	int val2 = -1;
@@ -97,6 +115,8 @@ int main()
 	{
 		assert(*i == val2);
 	}
+
+	std::cout << std::endl << "push front move ...";
 	int val4 = -2;
 	list10.push_front(std::move(val4));
 	assert(list10.size() == 7);
@@ -107,6 +127,8 @@ int main()
 	}
 	assert(val4 == 0);
 
+
+	std::cout << std::endl << "pop front ...";
 	list10.pop_front();
 	assert(list10.size() == 6);
 	int val5 = -1;
@@ -115,6 +137,7 @@ int main()
 		assert(*i == val5);
 	}
 
+	std::cout << std::endl << "push back move ...";
 	int val6 = 5;
 	list10.push_back(std::move(val6));
 	assert(list10.size() == 7);
