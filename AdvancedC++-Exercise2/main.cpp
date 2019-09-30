105aeffdc5dd6a3dcc121b36b9116dcbb3a43df4
lucaspadoni <lucaspado@hotmail.it>
2019-09-30 13:02:28 +0200

    Iterator fix

--- AdvancedC++-Exercise2/main.cpp
+++ AdvancedC++-Exercise2/main.cpp
@@ -1,7 +1,24 @@
+#include "SList.h"
 #include <iostream>
+#include <cassert>
 
+using namespace slist;
 int main()
 {
-	std::cout << "Hello world!" << std::endl;
+	SList<int> list;
+	// Add few items to the end of LinkedList 
+	list.push_back(111);
+	list.push_back(222);
+	list.push_back(333);
+
+	std::cout << "Iterator operator++ test... ";
+	SList<int>::iterator biterator = list.begin();
+	assert(*biterator++ == 111);
+	assert(*biterator++ == 222);
+	assert(*biterator++ == 333);
+	std::cout << "OK!" << std::endl;
+
+	std::cout << std::endl;
+
 	return 0;
 }
