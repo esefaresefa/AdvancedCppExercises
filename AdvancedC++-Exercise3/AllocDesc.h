fe1d53472a303474579f3140684349e9b6789ea2
lucaspadoni <lucaspado@hotmail.it>
2019-10-02 15:09:44 +0200

    A buon punto

--- AdvancedC++-Exercise3/AllocDesc.h
+++ AdvancedC++-Exercise3/AllocDesc.h
@@ -4,6 +4,7 @@
 
 struct AllocDesc
 {
+public:
 	// Allocation address
 	void* Ptr;
 
@@ -14,7 +15,16 @@ struct AllocDesc
 	tU32 AllocationType;
 
 	// Descripiton string
-	std::string Description;
+	const tChar* Description;
+
+	// File of the allocation
+	const tChar* File;
+
+	// Code line of the allocation
+	tU32 Line;
+
+	// Allocation Freed
+	bool Freed;
 
 	// Next allocation
 	AllocDesc* Next;
--- AdvancedC++-Exercise3/MMConfig.h
+++ AdvancedC++-Exercise3/MMConfig.h
@@ -2,10 +2,12 @@
 
 #include <iostream>
 
+// Defining types
+typedef char tChar;
+typedef uint32_t tU32;
+typedef uint64_t tU64;
+
 // Small Object Allocator configuration
 const size_t CHUNK_SIZE = 64;
 const size_t MAX_OBJECT_SIZE = 8;
 
-// Defining types
-typedef char tChar;
-typedef uint32_t tU32;
